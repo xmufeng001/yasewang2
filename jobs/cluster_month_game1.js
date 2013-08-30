e418123dd3acc2a1a4bb41d2a349c64d18b66103
xmufeng001 <xmufeng001@gmail.com>
2013-08-30 23:39:21 +0800

    312312312

--- jobs/cluster_month_game1.js
+++ jobs/cluster_month_game1.js
@@ -71,11 +71,10 @@ if (cluster.isMaster) {
 } else {
     process.on('message', function(userConf) {
         if(userConf.status==7){
-            var timeOutMinutes=userConf["resetInterval"]?userConf["resetInterval"]:-30;
+            var timeOutMinutes=userConf["resetInterval"]?userConf["resetInterval"]:-300;
             leveling.startJob(userConf,new Date(),timeOutMinutes,function(startTaskTime){
                 console.log(userConf["login_id"]+"  "+startTaskTime+"'s task is complete");
                 process.send({ cmd: 'reset' });
-                return;
             });
         }
     });
