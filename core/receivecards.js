ca37464706bb104ac8a29280f63d6649fa86f840
wind wu <xmufeng001@gmail.com>
2013-09-04 00:01:48 +0800

    1111111111
    11111

--- core/receivecards.js
+++ core/receivecards.js
@@ -9,18 +9,15 @@ var xmlreader = require("xmlreader");
 var userInfo = require("./userInfo");
 
 
-var startSaleCards = function (userConf) {
+var startReceivecards = function (userConf) {
 
     async.waterfall([
         function (callback) {
             login(userConf, callback);
         } ,
-        function (userConf, userData, willSaleCards, callback) {
+        function (userConf, userData, callback) {
+            receivecards(userConf, userData);
 
-            for(var i = 0; i < willSaleCards.length; i=i+30){
-                var subWillSaleCards=willSaleCards.slice(i,30);
-                saleCard(userConf, userData, subWillSaleCards);
-            }
         }
 
     ], function (err, result) {
@@ -66,34 +63,8 @@ var login = function (userConf, callback) {
                         var friendship_point = parseInt(result.response.header.your_data.friendship_point.text());
                         var user_id = result.response.body.login.user_id.text();
                         var userData = {name: name, town_level: town_level, gold: gold, ap: ap, bc: bc, friendship_point: friendship_point, user_id: user_id, host: host, login_id: login_id, session_id: session_id, cookie: "S=" + session_id};
-                        var user_card_list = result.response.header.your_data.owner_card_list.user_card;
 
-                        var willSaleCards = [];
-                        for (var i = 1; i < user_card_list.count(); i++) {
-                            var serial_id = user_card_list.at(i).serial_id.text();
-                            var master_card_id = user_card_list.at(i).master_card_id.text();
-                            var holography = user_card_list.at(i).holography.text();
-                            var hp = user_card_list.at(i).hp.text();
-                            var power = user_card_list.at(i).power.text();
-                            var critical = user_card_list.at(i).critical.text();
-                            var lv = user_card_list.at(i).lv.text();
-                            var lv_max = user_card_list.at(i).lv_max.text();
-                            var exp = user_card_list.at(i).exp.text();
-                            var max_exp = user_card_list.at(i).max_exp.text();
-                            var next_exp = user_card_list.at(i).next_exp.text();
-                            var exp_diff = user_card_list.at(i).exp_diff.text();
-                            var exp_per = user_card_list.at(i).exp_per.text();
-                            var sale_price = user_card_list.at(i).sale_price.text();
-                            var material_price = user_card_list.at(i).material_price.text();
-                            var evolution_price = user_card_list.at(i).evolution_price.text();
-                            var plus_limit_count = user_card_list.at(i).plus_limit_count.text();
-                            var limit_over = user_card_list.at(i).limit_over.text();
-                            if (lv == "1" && sale_price < 600) { //出售一星和二星卡
-                                willSaleCards.push(serial_id);
-                            }
-                        }
-
-                        callback(null, userConf, userData, willSaleCards);
+                        callback(null, userConf, userData);
                     });
 
 
@@ -109,20 +80,17 @@ var login = function (userConf, callback) {
     );
 }
 
-var saleCard = function (userConf, userData, willSaleCards, callback) {
-    console.log(userConf["login_id"] + ":sell");
+var receivecards = function (userConf, userData) {
+    console.log(userConf["login_id"] + ":fairyrewards");
     var login_id = userConf["login_id"];
     var host = userConf["host"];
     var headers = userInfo.browser_headers;
     headers["Host"] = host ? host : userInfo.game1_host;
     headers["Cookie"] = userData["cookie"];
-    var uri = "connect/app/trunk/sell?cyt=1";
-    var serial_id_list_string = willSaleCards.join(',');
-    var postDate = {serial_id: serial_id_list_string}; //serial_id=12085861&user_id=291
+    var uri = "connect/app/menu/fairyrewards?cyt=1";
     var requestObject = {
         headers: headers,
-        uri: "http://" + headers["Host"] + "/" + uri,
-        body: querystring.stringify(postDate)
+        uri: "http://" + headers["Host"] + "/" + uri
     };
     if (app_conf["proxy"]) {
         requestObject["proxy"] = "http://" + app_conf["proxyAddress"] + ":" + app_conf["proxyPort"]
@@ -154,8 +122,8 @@ var saleCard = function (userConf, userData, willSaleCards, callback) {
     );
 }
 
-exports.startSaleCards = function (userConf) {
-    startSaleCards(userConf);
+exports.startReceivecards = function (userConf) {
+    startReceivecards(userConf);
 }
 
 
---
+++ jobs/salecards.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var fs = require('fs');
+var salecard =require('../core/salecard.js');
+
+var getAllUserConf=function(){
+    var game1_common_account_string=fs.readFileSync("../conf/game1-CBT_common_account.json") ;
+    var game1_common_account_list=JSON.parse(game1_common_account_string)["accounts"];
+    var game1_vip_account_string=fs.readFileSync("../conf/game1-CBT_vip_account.json") ;
+    var game1_vip_account_list=JSON.parse(game1_vip_account_string)["accounts"];
+    var game1_month_account_string=fs.readFileSync("../conf/game1-CBT_month_account.json") ;
+    var game1_month_account_list=JSON.parse(game1_month_account_string)["accounts"];
+    var game2_common_account_string=fs.readFileSync("../conf/game2-CBT_common_account.json") ;
+    var game2_common_account_list=JSON.parse(game2_common_account_string)["accounts"];
+    var game2_vip_account_string=fs.readFileSync("../conf/game2-CBT_vip_account.json") ;
+    var game2_vip_account_list=JSON.parse(game2_vip_account_string)["accounts"];
+    var all_account_list=[];
+    all_account_list=all_account_list.concat(game1_common_account_list);
+    all_account_list=all_account_list.concat(game1_vip_account_list);
+    all_account_list=all_account_list.concat(game1_month_account_list);
+    all_account_list=all_account_list.concat(game2_common_account_list);
+    all_account_list=all_account_list.concat(game2_vip_account_list);
+    return all_account_list;
+};
+var getUserByLoginId=function(login_Id){
+    var userConfList= getAllUserConf();
+    for(var i= 0;i<userConfList.length;i++){
+        if(userConfList[i]["login_id"]==login_Id){
+            return userConfList[i];
+        }
+    }
+    return null;
+};
+
+
+var userConfList=getAllUserConf();
+userConfList.forEach(function(userConf) {
+    if(userConf["login_id"]=="15820263794"){
+        salecard.startSaleCards(userConf);
+    }
+});
+
